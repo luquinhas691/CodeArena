1d879c80ef136a53ad74f23a4947b23d452309c6
Adan Bandim <adanlima11@gmail.com>
2026-06-01 21:45:45 -0300

    Add files via upload

---
+++ scr/Character.java
@@ -0,0 +1,23 @@
+package character;
+
+public abstract class Character {
+    protected String nome;
+    protected int vida;
+    protected int vidaMaxima;
+    protected int forca;
+
+    public Character(String nome, int vida, int forca) {
+        this.nome = nome;
+        this.vida = vida;
+        this.vidaMaxima = vida;
+        this.forca = forca;
+    }
+
+    public boolean estaVivo() { return vida > 0; }
+
+    public String getNome() { return nome; }
+    public int getVida() { return vida; }
+    public void setVida(int vida) { this.vida = Math.max(0, Math.min(vida, vidaMaxima)); }
+    public int getForca() { return forca; }
+    public void setForca(int forca) { this.forca = forca; }
+}
